f385add02a44df57ed97b86024380d345f93222b
Dany Cambrano <danydevtest@gmail.com>
2021-06-30 16:09:06 -0500

    Se agregaron los middlewares para las conexiones http cors y babel

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
+//const express = require('express');
+import express from 'express';
+//const path = require('path');
+import path from 'path';
+//const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+//const cors = require('cors');
+import cors from 'cors';
 
 const app=express();
-
 
 mongoose.Promise=global.Promise;
 const dbURL = 'mongodb://localhost:27017/';
@@ -12,6 +16,7 @@ mongoose.connect(dbURL, {useUnifiedTopology: true, useNewUrlParser: true, useCre
 .catch(err=>console.log(err));
 
 app.set('port', process.env.PORT || 4000);
+app.use(cors());
 
 
 app.use(express.json());
