1cb31889f7184872d3d9309b757c7b3497dbe96d
Dany Cambrano <danydevtest@gmail.com>
2021-07-07 15:34:50 -0500

    Se agregaron las funciones para listarUno eliminar y actualizar en el controlador de personas

--- controller/persona.controller.js
+++ controller/persona.controller.js
@@ -1,39 +1,94 @@
-import model from '../models';
+import model from "../models";
 
 export default {
-    agregar: async(req, res, next)=>{
-        try{
-            const {nombre, apellidos, direccion}=req.body;
-
-            const personas= new model.Persona({
-                nombre,
-                apellidos,
-                direccion
-            });
-
-            const registro = await personas.save();
-            res.status(200).json(registro);
-            console.log(req.body);
-        }catch(e){
-            res.status(500).send({
-                message: "Ocurrio un error al guardar los datos"
-            });
-
-            next(e);
-        }
-    },
-
-    consultar: async(req,res, next)=>{
+  agregar: async (req, res, next) => {
     try {
-        const personas= await model.Persona.find();
-        console.log(personas);
-        res.json(personas);
-    } catch (error) {
-        res.status(500).send({
-            message: "Ocurrio un error al consultar"
-        });
+      const { nombre, apellidos, direccion } = req.body;
 
-        next(error);
+      const personas = new model.Persona({
+        nombre,
+        apellidos,
+        direccion,
+      });
+
+      const registro = await personas.save();
+      res.status(200).json(registro);
+      console.log(req.body);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocurrio un error al guardar los datos",
+      });
+
+      next(e);
     }
+  },
+
+  consultar: async (req, res, next) => {
+    try {
+      const personas = await model.Persona.find();
+      console.log(personas);
+      res.json(personas);
+    } catch (error) {
+      res.status(500).send({
+        message: "Ocurrio un error al consultar",
+      });
+
+      next(error);
     }
-}
+  },
+
+  consultarUno: async (req, res, next) => {
+    try {
+      const consulta = await model.Persona.findById(req.params.id);
+
+      if (!consulta) {
+        res.status(404).send({
+          message: "El registro con el id solicitado no existe",
+        });
+      } else {
+        res.status(200).json(consulta);
+      }
+    } catch (e) {
+      res.status(500).send({
+        message: "El id no existe",
+      });
+      next(e);
+    }
+  },
+
+  eliminar:async(req, res, next)=>{
+      try {
+
+        const eliminarPersona= await model.Persona.findByIdAndDelete(req.params.id);
+        res.status(200).send({
+            message: "La informacion ha sido eliminada correctamente"
+        });
+          
+      } catch (error) {
+          res.status(500).send({
+              message: "El id para eliminar persona no existe"
+          });
+
+          next(error);
+      }
+  },
+
+  Actualizar: async(req, res, next)=>{
+      try {
+          const {nombre, apellidos, direccion}=req.body;
+
+          const actualizarPersona={
+              nombre,apellidos,direccion
+          }
+
+          const datosActuales = await model.Persona.findByIdAndUpdate(req.params.id, actualizarPersona);
+          res.json(actualizarPersona);
+
+      } catch (error) {
+          res.status(500).send({
+              message:"Ocurrio un error al tratar de actualizar"
+          });
+          next(error);
+      }
+  }
+};
--- routes/persona.route.js
+++ routes/persona.route.js
@@ -1,4 +1,5 @@
 import routerx from 'express-promise-router';
+import personaController from '../controller/persona.controller';
 
 //const routerx = require('express-promise-router');
 
@@ -9,6 +10,9 @@ const router=routerx();
 
 router.post('/crearPersona', PersonaController.agregar);
 router.get('/listarPersonas', PersonaController.consultar);
+router.get('/listarPersona/:id', PersonaController.consultarUno );
+router.delete('/eliminarPersona/:id', personaController.eliminar);
+router.put('/actualizarPersona/:id', PersonaController.Actualizar);
 
 
 export default router;
